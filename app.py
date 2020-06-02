5ea8d903efeec5d72ace71ca296df6d78c44e5f9
tmitchell <tmitchell@chapman.edu>
2020-06-02 12:01:28 -1000

    completed

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import tkinter as tk
 from tkinter import filedialog, Text
+import os
 import subprocess
-
 
 #main
 root = tk.Tk()
 #create list
 apps = []
+
+#read in save.txt file
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        #gets text from document and saves as string
+        tempApps = f.read()
+        #splits file by commas
+        tempApps = tempApps.split(',')
+        #gets rid of empty spaces when window is closed
+        apps = [x for x in tempApps if x.strip()]
 
 def addApp():
     #clear frame
@@ -46,4 +56,14 @@ runApps = tk.Button(root, text = "Run Apps", padx = 10, pady = 5, fg="black",
                     bg="#33F9FF", command=runApps)
 runApps.pack()
 
+#show label from read in file
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
 root.mainloop()
+
+#serialize Apps
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
